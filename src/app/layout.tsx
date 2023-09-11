88b2ec98a054ea0917c1d9e7b9da3a6f6bef5c43
Come Grellard <come.grellard@ledger.fr>
2023-09-11 15:39:38 +0200

    formatted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import TransportProvider from "@/components/TransportProvider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,11 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <TransportProvider>
-        {children}
-        </TransportProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
