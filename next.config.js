3014fc49ff98f9b5c2be33fce5155cd4c6f3edef
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2023-09-12 14:53:46 +0200

    Add semi (#8)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/TransportProvider.tsx
+++ src/components/TransportProvider.tsx
@@ -1,6 +1,6 @@
-"use client"
-import { WalletAPIProvider } from "@ledgerhq/wallet-api-client-react"
-import { Transport, WindowMessageTransport } from "@ledgerhq/wallet-api-client"
+"use client";
+import { WalletAPIProvider } from "@ledgerhq/wallet-api-client-react";
+import { Transport, WindowMessageTransport } from "@ledgerhq/wallet-api-client";
 
 function TransportProvider({ children }: { children: React.ReactElement }) {
   function getWalletAPITransport(): Transport {
@@ -8,17 +8,17 @@ function TransportProvider({ children }: { children: React.ReactElement }) {
       return {
         onMessage: undefined,
         send: () => {},
-      }
+      };
     }
 
-    const transport = new WindowMessageTransport()
-    transport.connect()
-    return transport
+    const transport = new WindowMessageTransport();
+    transport.connect();
+    return transport;
   }
 
-  const transport = getWalletAPITransport()
+  const transport = getWalletAPITransport();
 
-  return <WalletAPIProvider transport={transport}>{children}</WalletAPIProvider>
+  return <WalletAPIProvider transport={transport}>{children}</WalletAPIProvider>;
 }
 
-export default TransportProvider
+export default TransportProvider;
