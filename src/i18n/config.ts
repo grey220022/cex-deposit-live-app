510614b384caa4dc78474db58e6c1ea07bdcd510
Maxime Aubanel <sshmaxime@gmail.com>
2023-09-21 10:05:17 +0200

    Remove useless comments + re arrange type var

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,18 +3,16 @@
  */
 export const LOCALES = ["en", "fr"] as const;
 
-/**
- * Default Locale.
- */
-export const DEFAULT_LOCALE: Locale = "en";
+export type Locale = (typeof LOCALES)[number];
+export type LocaleInfo = { file: File };
+export type LocaleMap<T = string> = { [key in Locale]: T };
 
-/**
- * Mapping from locales to their respective infos.
- */
 export const Locales = {
   en: { file: () => import("./locales/en.json") },
   fr: { file: () => import("./locales/fr.json") },
 } as const satisfies LocaleMap<LocaleInfo>;
+
+export const DEFAULT_LOCALE: Locale = "en";
 
 /**
  * Mapping from locales to their respective i18n files.
@@ -28,7 +26,4 @@ export const LocalesI18nConfig = Object.values(LOCALES).reduce(
  * Utils type.
  */
 export type File = () => Promise<unknown>;
-export type Locale = (typeof LOCALES)[number];
-export type LocaleInfo = { file: File };
-export type LocaleMap<T = string> = { [key in Locale]: T };
 export type LocaleSchema = (typeof Locales)[typeof DEFAULT_LOCALE]["file"];
